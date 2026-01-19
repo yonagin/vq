49a38a28644c52cf1d5804369c1346abba733281
yonagin <3118391854@qq.com>
2026-01-20 00:15:15 +0800

    init

--- ldm/modules/diffusionmodules/openaimodel.py
+++ ldm/modules/diffusionmodules/openaimodel.py
@@ -318,7 +318,7 @@ class AttentionBlock(nn.Module):
     def _forward(self, x):
         b, c, *spatial = x.shape
         x = x.reshape(b, c, -1)
-        with torch.cuda.amp.autocast(enabled=True):
+        with th.cuda.amp.autocast(enabled=True):
             qkv = self.qkv(self.norm(x))
             h = self.attention(qkv)
             h = self.proj_out(h)
